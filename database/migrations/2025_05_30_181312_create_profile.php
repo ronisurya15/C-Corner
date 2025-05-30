6c66cb10aa151b59a163d34a93b42bd8d5a8389c
roofi28 <roofitkj@gmail.com>
2025-05-31 01:17:57 +0700

    profile migration

---
+++ database/migrations/2025_05_30_181312_create_profile.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profile', function (Blueprint $table) {
+            $table->id();
+            $table->full_name();
+            $table->gender();
+            $table->email();
+            $table->university_name();
+            $table->majors();
+            $table->prodi();
+            $table->date_of_birth();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profile');
+    }
+};
