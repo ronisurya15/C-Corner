3ad3f06d6d347a793395b16277deb3cf2530d1e3
Rivalaly28 <rivalalymurviqin28@gmail.com>
2025-06-04 22:38:30 +0700

    input jurusan

--- resources/views/ForumGroups/tambah.blade.php
+++ resources/views/ForumGroups/tambah.blade.php
@@ -4,31 +4,28 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Forum</title>
+    <title>Tambah Forum Jurusan</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 </head>
 
-<body>
-     <div class="bg-white p-8 rounded shadow-md w-full max-w-sm">
-        <h2 class="text-2xl font-bold mb-6 text-center">Sign In</h2>
+<body class="bg-gray-900 text-white font-sans">
+    <div class="min-h-screen flex items-center justify-center">
+        <div class="bg-gray-800 p-6 rounded-lg shadow-lg w-full max-w-md">
+            <h2 class="text-2xl font-bold mb-6">Forum Jurusan</h2>
+            
+                @csrf
 
-        <form action="" method="POST">
-            @csrf
-            <div class="mb-4">
-                <label class="block mb-1 font-semibold">Email:</label>
-                <input type="email" name="email" class="w-full border border-gray-300 px-3 py-2 rounded focus:outline-none focus:ring focus:border-blue-500" required>
-            </div>
+               
 
-            <div class="mb-4">
-                <label class="block mb-1 font-semibold">Password:</label>
-                <input type="password" name="password" class="w-full border border-gray-300 px-3 py-2 rounded focus:outline-none focus:ring focus:border-blue-500" required>
-            </div>
+                {{-- Nama Jurusan --}}
+                <div class="mb-4">
+                    <label class="block mb-1 font-semibold">Nama Jurusan:</label>
+                    <input type="text" name="jurusan_forum" placeholder=""
+                        class="w-full px-4 py-2 bg-gray-700 text-white rounded-lg focus:outline-none" required>
+                </div>
 
-            <button type="submit" class="w-full bg-blue-600 hover:bg-blue-500 text-white py-2 rounded">Sign In</button>
-        </form>
-
-        <div class="mt-4 text-sm text-center">
-            <a href="#" class="text-blue-600 hover:underline">Sign Up</a> |
-            <a href="#" class="text-blue-600 hover:underline">Forgot Password?</a>
+                
+            </form>
         </div>
     </div>
 </body>
